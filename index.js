cc63ee0909857780c254a01b5323797c6b02daba
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-04-12 03:31:54 +0600

    new

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ navCloseBtn.addEventListener('click', navToggleFunc);
 // theme toggle variables
 const themeBtn = document.querySelectorAll('.theme-btn');
 
-for (let i = 0; i < themeBtn.length; i++) {
-	themeBtn[i].addEventListener('click', function () {
+for (let x = 0; x < themeBtn.length; x++) {
+	themeBtn[x].addEventListener('click', function () {
 		// toggle `light-theme` & `dark-theme` class from `body`
 		// when clicked `theme-btn`
 		document.body.classList.toggle('light-theme');
 		document.body.classList.toggle('dark-theme');
 
-		for (let i = 0; i < themeBtn.length; i++) {
+		for (let x = 0; x < themeBtn.length; x++) {
 			// When the `theme-btn` is clicked,
 			// it toggles classes between `light` & `dark` for all `theme-btn`.
-			themeBtn[i].classList.toggle('light');
-			themeBtn[i].classList.toggle('dark');
+			themeBtn[x].classList.toggle('light');
+			themeBtn[x].classList.toggle('dark');
 		}
 	});
 }
