fec826f35c152673e3859397559f6411c1f785d7
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-04-15 01:43:19 +0600

    updated

--- index.js
+++ index.js
@@ -35,3 +35,20 @@ for (let x = 0; x < themeBtn.length; x++) {
 }
 
 // second part fo javascript start
+
+const modal = document.getElementById('myModal');
+
+const img = document.getElementById('images_modal');
+const modalImg = document.getElementById('img01');
+const captionText = document.getElementById('caption');
+img.onclick = function () {
+	modal.style.display = 'block';
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+};
+
+const span = document.getElementsByClassName('close')[0];
+
+span.onclick = function () {
+	modal.style.display = 'none';
+};
