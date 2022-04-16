ab061a0323bc6df5c30cb732d1cce1b692f4f697
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-04-16 16:40:24 +0600

    updated

--- index.js
+++ index.js
@@ -73,3 +73,35 @@ function scrollFunction() {
 
 // year update by default
 document.getElementById('newYear').innerHTML = new Date().getFullYear();
+
+// extra hero section start
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(arrow) {
+	showSlides((slideIndex += arrow));
+}
+
+function currentSlide(arrow) {
+	showSlides((slideIndex = arrow));
+}
+
+function showSlides(arrow) {
+	let i;
+	let slides = document.getElementsByClassName('mySlides');
+	let dots = document.getElementsByClassName('demo');
+	if (arrow > slides.length) {
+		slideIndex = 1;
+	}
+	if (arrow < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(' active', '');
+	}
+	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].className += ' active';
+}
